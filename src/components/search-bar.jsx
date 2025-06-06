6da5aa8b9fba7e90c30dbc439bdf46ddc67a7e52
Ben Cuff <bcuff@meta.com>
2025-06-06 13:31:57 -0700

    Update .gitignore, enhance SearchBar component, and add styles

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import "./search-bar.css";
+
 export default function SearchBar() {
+	const [searchInput, setSearchInput] = useState("");
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setSearchInput("");
+	};
+
 	return (
-		<form>
-			<input placeholder="Search for movies"></input>
-			<button type="">Submit</button>
-			<button>Clear</button>
+		<form onSubmit={handleSubmit}>
+			<input
+				placeholder="Search for movies"
+				value={searchInput}
+				className="search-input"
+				onChange={(event) => {
+					setSearchInput(event.target.value);
+				}}
+			></input>
+			<button className="search-btn" type="submit">
+				Submit
+			</button>
+			<button className="search-btn" onClick={() => setSearchInput("")}>
+				Clear
+			</button>
 		</form>
 	);
 }
