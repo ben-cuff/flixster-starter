d63827b63c06bc1f7b43e29f0603be7588cdf378
Ben Cuff <bencuff14@gmail.com>
2025-06-12 09:57:21 -0700

    move accessToken declaration to the top of movieApi for better scope management

--- src/api/movie-api.js
+++ src/api/movie-api.js
@@ -1,6 +1,7 @@
+const accessToken = import.meta.env.VITE_IMDB_ACCESS_TOKEN;
+
 export const movieApi = {
 	searchMovies: async (query) => {
-		const accessToken = import.meta.env.VITE_IMDB_ACCESS_TOKEN;
 		const response = await fetch(
 			`https://api.themoviedb.org/3/search/movie?query=${query}`,
 			{
@@ -16,7 +17,6 @@ export const movieApi = {
 		return data;
 	},
 	getPage: async (query) => {
-		const accessToken = import.meta.env.VITE_IMDB_ACCESS_TOKEN;
 		const response = await fetch(
 			`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${query}`,
 			{
@@ -32,7 +32,6 @@ export const movieApi = {
 		return data;
 	},
 	getMovieDetails: async (movieId) => {
-		const accessToken = import.meta.env.VITE_IMDB_ACCESS_TOKEN;
 		const response = await fetch(
 			`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
 			{
@@ -48,7 +47,6 @@ export const movieApi = {
 		return data;
 	},
 	getMovieVideos: async (movieId) => {
-		const accessToken = import.meta.env.VITE_IMDB_ACCESS_TOKEN;
 		const response = await fetch(
 			`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
 			{
