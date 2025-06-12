95b823db1c7cc21b803094170f94cd0cfe1ccfc6
Ben Cuff <bencuff14@gmail.com>
2025-06-12 09:44:52 -0700

    refactor MovieCard component to improve readability and structure

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -6,56 +6,64 @@ import MovieModal from "./movie-modal";
 export default function MovieCard({ movie, setMovieData }) {
 	const [toggleModal, setToggleModal] = useState(false);
 
-	if (movie) {
-		return (
-			<>
-				{toggleModal && (
-					<MovieModal setToggleModal={setToggleModal} movie={movie} />
-				)}
-				<div
-					className="movie-card"
-					onClick={() => setToggleModal(!toggleModal)}
-				>
-					<img
-						src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
-						alt={movie.title}
-					/>
-					<span className="icon-img">
-						<img
-							src={movie.liked ? "heart.svg" : "heart-outline.svg"}
-							alt="liked status"
-							onClick={(e) => {
-								e.stopPropagation();
-								setMovieData((prevMovies) =>
-									prevMovies.map((m) =>
-										m.id === movie.id ? { ...m, liked: !m.liked } : m
-									)
-								);
-							}}
-						/>
-
-						<h4>{movie.title}</h4>
-						<img
-							src={
-								movie.watched ? "eye-outline.svg" : "eye-off-outline.svg"
-							}
-							alt="watched status"
-							className="icon-img"
-							onClick={(e) => {
-								e.stopPropagation();
-								setMovieData((prevMovies) =>
-									prevMovies.map((m) =>
-										m.id === movie.id ? { ...m, watched: !m.watched } : m
-									)
-								);
-							}}
-						/>
-					</span>
-					<p>Rating: {movie.vote_average}</p>
-				</div>
-			</>
-		);
+	if (movie == null) {
+		return <p>No movie data</p>;
 	}
+
+	return (
+		<>
+			{toggleModal && (
+				<MovieModal setToggleModal={setToggleModal} movie={movie} />
+			)}
+			<div
+				className="movie-card"
+				onClick={() => setToggleModal(!toggleModal)}
+			>
+				<img
+					src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+					alt={movie.title}
+				/>
+				<span className="icon-img">
+					<img
+						src={movie.liked ? "heart.svg" : "heart-outline.svg"}
+						alt="liked status"
+						onClick={(e) => {
+							e.stopPropagation();
+							setMovieData((prevMovies) =>
+								prevMovies.map((m) =>
+									m.id === movie.id
+										? { ...m, liked: !m.liked }
+										: m
+								)
+							);
+						}}
+					/>
+
+					<h4>{movie.title}</h4>
+					<img
+						src={
+							movie.watched
+								? "eye-outline.svg"
+								: "eye-off-outline.svg"
+						}
+						alt="watched status"
+						className="icon-img"
+						onClick={(e) => {
+							e.stopPropagation();
+							setMovieData((prevMovies) =>
+								prevMovies.map((m) =>
+									m.id === movie.id
+										? { ...m, watched: !m.watched }
+										: m
+								)
+							);
+						}}
+					/>
+				</span>
+				<p>Rating: {movie.vote_average}</p>
+			</div>
+		</>
+	);
 }
 
 MovieCard.propTypes = {
